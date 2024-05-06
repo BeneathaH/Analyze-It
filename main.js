1d09bef50a67e99af748c28300982b960ff59019
Beneatha Hayes <you@example.com>
2024-05-05 22:27:54 -0500

    continued

--- main.js
+++ main.js
@@ -2,8 +2,11 @@ const apiKey = "f29ababb7a5886470525f8e599bbc143"
 
 const appId = "42b9b596"
 
+// this is my nutrition analysis API and URL section from EDAMAM.
 
 const nutritionURL = "https://api.edamam.com/api/nutrition-data?app_id=" +  appId + "&app_key=" + apiKey + "&ingr= 1 cup "; 
+
+// This is the Javascript code for the submit button, the advent listener listens for the nutrition intake click.
 
 const submitButton = document.getElementById("ingrBtn")
 submitButton.addEventListener("click", (event) => {
@@ -12,6 +15,8 @@ submitButton.addEventListener("click", (event) => {
 console.log(ingrInputValue)
     fetch(nutritionURL + ingrInputValue)
     .then(response => response.json())
+
+    // this is the Javascript code for the nutrition analysis API. I pulled out specific things to highlight like fat kcal, protein, kcal, and carbohydrates kcal.
     
     .then((data) => {
         const {calories, healthLabels, totalNutrientsKCal: {CHOCDF_KCAL, FAT_KCAL, PROCNT_KCAL }}= data
@@ -32,14 +37,16 @@ console.log(ingrInputValue)
         ingrResults.appendChild(healthLabel)
         ingrResults.appendChild(healthValue)
 
-        const caloriesPerCarbo = document.createElement("h3");
+        // This Javascript code pulls the Carbohydrates.
+        const caloriesPerCarbo = document.createElement("h3");      
         caloriesPerCarbo.innerText = "Total Nutrients"
-        const caloriesPerCarboValue = document.createElement('span')
+        const caloriesPerCarboValue = document.createElement('span')  
         caloriesPerCarbo.innerText = CHOCDF_KCAL.label
         caloriesPerCarboValue.innerText = CHOCDF_KCAL.quantity
         ingrResults.appendChild(caloriesPerCarbo)
         ingrResults.appendChild(caloriesPerCarboValue)
 
+        // This Javascript code pulls the Fat.
         const caloriesPerFat = document.createElement("h3");
         caloriesPerFat.innerText = "Total Nutrients"
         const caloriesPerFatValue = document.createElement('span')
@@ -48,6 +55,7 @@ console.log(ingrInputValue)
         ingrResults.appendChild(caloriesPerFat)
         ingrResults.appendChild(caloriesPerFatValue)
 
+        // This Javascript code pulls the Protein.
         const caloriesPerPro = document.createElement("h3");
         caloriesPerPro.innerText = "Total Nutrients"
         const caloriesPerProValue = document.createElement('span')
